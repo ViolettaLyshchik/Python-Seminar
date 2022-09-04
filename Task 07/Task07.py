b1ed3226eb3dac6a611e0368ee5e828419baa5c7
Violetta <viola.kotovich@gmail.com>
2022-09-04 12:44:36 +0300

    Д/З проверка истиности утверждения для всех значений предикат

---
+++ Task 07/Task07.py
@@ -0,0 +1,9 @@
+#Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+for X in range(2):
+    for Y in range(2):
+        for Z in range(2):
+            if (not(X or Y or Z)) == (not X and not Y and not Z):
+               print('Истина')
+            else:
+               print('Ложь')

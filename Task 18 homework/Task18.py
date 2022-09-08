b0405a6daf7b6f40928a5950cf24b7b1c79d7e92
Violetta <viola.kotovich@gmail.com>
2022-09-08 15:32:05 +0300

    д/з сумма из n чисел последовательности

--- Task 18 homework/Task18.py
+++ Task 18 homework/Task18.py
@@ -3,4 +3,4 @@ n = int(input("Введите число n: "))
 sum = 0
 for i in range(1,n+1):
     sum += (1+1/i)**i
-print(f"Cумма из n чисел последовательности равнна {round(sum,2)}")
+print(f"Cумма последовательности (1+1/n)^n = {round(sum,2)}")

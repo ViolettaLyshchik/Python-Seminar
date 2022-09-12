4bc68c5fdba64bddf7ba49c691a2cd44fed9b274
Violetta <viola.kotovich@gmail.com>
2022-09-12 14:50:15 +0300

    Произведение пар чисел в списке

--- Task28_sem3_homework/sem3_homework.py
+++ Task28_sem3_homework/sem3_homework.py
@@ -6,9 +6,9 @@
 list = [2,3,4,5,6]
 print(list)
 
-def product(list):
+def mult(list):
     mult = []
     for i in range((len(list)+1)//2):
         mult.append(list[i]*list[len(list)-1-i])
     return mult
-print(product(list))
+print(mult(list))

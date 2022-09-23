b6a9e35973283dacb8b0cda78515ad7b830333c7
Violetta <viola.kotovich@gmail.com>
2022-09-23 12:53:15 +0300

    Семинар 6

---
+++ Seminar_6/Task2.py
@@ -0,0 +1,11 @@
+#Дан список, вывести отдельно буквы и цифры.
+#a = ( "a", 'b', '2', '3' ,'c')
+#b = ( 'a' , 'b' , 'c')
+#c = ( '1', '2', '3')
+import numbers
+
+
+lst = ["a", 'b', '2', '3', 'c']
+word = list(filter(lambda x: x.isdigit(), lst))
+number = list(filter(lambda x: x.isalpha(), lst))
+print(word, number)
